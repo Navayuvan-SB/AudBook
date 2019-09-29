527ebbf94ef096866fa3782f0a4e6fd9b227a889
navayuvan-sb <ns.navayuvan@gmail.com>
2019-09-29 14:28:22 +0530

    firebase initialize

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,11 @@
+export const Audi = {
+    Auditor: {
+        apiKey: "AIzaSyCCc7OjgEVYuD7OWgc2Chgvks1uN7aoN50",
+        authDomain: "audbook2k19.firebaseapp.com",
+        databaseURL: "https://audbook2k19.firebaseio.com",
+        projectId: "audbook2k19",
+        storageBucket: "",
+        messagingSenderId: "306454376311",
+        appId: "1:306454376311:web:ca51873d1ed656d3ae7b72"
+      } 
+}
