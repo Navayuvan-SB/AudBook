fe13799e857511b037485e0d706b87e7eb228256
navayuvan-sb <ns.navayuvan@gmail.com>
2019-12-15 01:27:23 +0530

    Auto-Login feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,60 @@ import { WarningPage } from '../pages/warning/warning';
 import { DashboardPage } from '../pages/dashboard/dashboard';
 import { RequestPage } from '../pages/request/request';
 import { LoginPage } from '../pages/login/login';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseServices } from '../services/fireBaseService';
 
- 
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any;
+
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private angularFire: AngularFireAuth,
+    private fbService: FirebaseServices) {
+
     platform.ready().then(() => {
+
+
+      this.angularFire.authState.subscribe(user => {
+
+        if (user) {
+
+          // Get the UID of Logged in user
+          let uid = user.uid;
+
+          // get the user type and navigate to according to it.
+          this.fbService.readOnce('users/' + uid)
+            .then((response) => {
+
+              // Check the user type and navigate to the apt page.
+              if (response.type == 'user') {
+
+                this.rootPage = StatusPage;
+
+              } else if (response.type == 'admin') {
+
+                this.rootPage = DashboardPage;
+
+              }
+            })
+            .catch((error) => {
+
+            });
+        }else{
+          this.rootPage = LoginPage;
+        }
+      });
+    
+
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
@@ -33,4 +76,3 @@ export class MyApp {
     });
   }
 }
-
