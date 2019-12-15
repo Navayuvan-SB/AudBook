eb4ed86992e589a2fae43e5852c8c94764108a29
navayuvan-sb <ns.navayuvan@gmail.com>
2019-12-15 09:36:14 +0530

    AutoLogin Enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { UloginPage } from '../pages/Ulogin/Ulogin';
 import { StatusPage } from '../pages/status/status';
 import { EditPage } from '../pages/Edit/Edit';
 import { CreatePage } from '../pages/create/create';
